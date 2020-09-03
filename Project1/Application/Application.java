b6814381bdf94efc1dafcde82236ac5c375b27e9
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 19:20:59 +0200

    drawing code

--- Project1/Application/Application.java
+++ Project1/Application/Application.java
@@ -4,7 +4,7 @@ import Classes.Program;
 
 public class Application {
     public static void main(String[] args){
-        Program program=new Program();
+        Program program=new Program(1000,600);
     }
 
 }
--- Project1/Classes/Program.java
+++ Project1/Classes/Program.java
@@ -5,15 +5,38 @@ import sum.kern.*;
 public class Program {
     Bildschirm screen;
     Stift pencil;
-    
-    public Program(int ){
-        screen=new Bildschirm();
-        pencil=new Stift();
+
+    public Program(int xSize,int ySize){
+            screen=new Bildschirm(xSize,ySize);
+            pencil=new Stift();
     }
 
-    public void sampleMethod(int x,int y)    {
+
+    public void square(int x,int y,int xSize,int ySize){
+        pencil.normal();
+        pencil.bewegeBis(x, y);
         pencil.runter();
-        pencil.bewegeBis(x,y);
-        
+        pencil.bewegeBis(x+xSize, y);
+        pencil.bewegeBis(x+xSize, y+ySize);
+        pencil.bewegeBis(x, y+ySize);
+        pencil.bewegeBis(x, y);
+
+
     }
+
+    public void circle(int x,int y, int radius){
+        pencil.normal();
+        pencil.bewegeBis(x, y+radius);
+        pencil.runter();
+        float angle=0;
+        for(int i=0;i<radius*10;i++){
+            angle=i*(float)Math.PI/(radius*5);
+            System.out.println(x+Math.sin(angle)*radius+"  "+y+Math.cos(angle)*radius);
+            pencil.bewegeBis(x+Math.sin(angle)*radius,y+Math.cos(angle)*radius);
+        }
+
+
+    }
+
 }
+
